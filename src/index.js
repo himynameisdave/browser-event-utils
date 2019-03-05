7de39d669794a508405d8fb92be609e22acd999f
himynameisdave <d@velunny.com>
2019-03-05 08:57:34 -0800

    👽 Properly export withTargetValue

--- src/index.js
+++ src/index.js
@@ -1,3 +1,4 @@
 export { default as withPreventDefault } from './with-prevent-default.js';
 export { default as withStopPropagation } from './with-stop-propagation.js';
 export { default as withStopImmediatePropagation } from './with-stop-immediate-propagation.js';
+export { default as withTargetValue } from './with-target-value.js';
