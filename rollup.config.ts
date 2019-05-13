07ef77e96b61002e6dba0830a4abcbc07ca522d8
himynameisdave <d@velunny.com>
2019-05-12 21:08:58 -0700

    🔩 Add typescript tooling
    Inspired heavily by:
    https://github.com/alexjoverm/typescript-library-starter

---
+++ rollup.config.ts
@@ -0,0 +1,41 @@
+import commonjs from 'rollup-plugin-commonjs';
+import json from 'rollup-plugin-json';
+import resolve from 'rollup-plugin-node-resolve';
+import sourceMaps from 'rollup-plugin-sourcemaps';
+import typescript from 'rollup-plugin-typescript2';
+
+// import pkg from './package.json';
+const pkg = require('./package.json');
+
+
+export default {
+    input: './src/index.js',
+    output: [
+        {
+            file: pkg.main,
+            name: 'browserEventUtils',
+            format: 'umd',
+            sourcemap: true
+        },
+        {
+            file: pkg.module,
+            format: 'es',
+            sourcemap: true
+        },
+    ],
+    plugins: [
+        json(),
+        typescript({
+            useTsconfigDeclarationDir: true
+        }),
+        commonjs(),
+        resolve(),
+        sourceMaps(),
+    ],
+    // // Indicate here external modules you don't wanna include in your bundle (i.e.: 'lodash')
+    // external: [],
+    // // Include if you want to add watch mode
+    // watch: {
+    //     include: 'src/**',
+    // },
+};
