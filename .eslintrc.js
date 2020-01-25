28f1993f9d22fd4ba144b512b63c0e700704d589
himynameisdave <dave@fossa.com>
2020-01-24 16:39:28 -0800

    📑 Configure TS eslint config

---
+++ .eslintrc.js
@@ -0,0 +1,27 @@
+module.exports = {
+  "extends": [
+    "himynameisdave/configurations/babel-node",
+    "himynameisdave/configurations/typescript"
+  ],
+  // "parser": "@typescript-eslint/parser",
+  "parserOptions": {
+    "sourceType": "module",
+    "tsconfigRootDir": __dirname,
+    "project": "./tsconfig.json"
+  },
+  "settings": {
+    "import/parsers": {
+      "@typescript-eslint/parser": [".ts"]
+    },
+    "import/extensions": [".ts"],
+    "import/resolver": {
+      "typescript": {
+        "alwaysTryTypes": true // always try to resolve types under `<roo/>@types` directory even it doesn't contain any source code, like `@types/unist`
+      },
+    },
+  },
+  "rules": {
+    "import/extensions": "off",
+    "jest/prefer-called-with": "off"
+  }
+}
